136cce572c2aee8d70784f0146cb349712064c03
Emre COŞKUN <emrecoskun06@hotmail.com>
2023-03-10 17:51:40 +0300

    modal componentleri ve buton componentleri oluşturuldu

--- MealOrdering.NetBlazor/Client/Program.cs
+++ MealOrdering.NetBlazor/Client/Program.cs
@@ -1,11 +1,18 @@
 using MealOrdering.NetBlazor.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.Modal;
+using MealOrdering.NetBlazor.Client.Utils;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<ModalManager>();
+
+builder.Services.AddBlazoredModal();
+//builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
